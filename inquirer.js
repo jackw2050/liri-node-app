da6340eb8737e86752fba7eeefed889839e05f69
Jack Walker <jackw2050@users.noreply.github.com>
2016-07-04 15:40:28 -0500

    Delete inquirer.js

--- inquirer.js
+++
@@ -1,7 +0,0 @@
-//https://www.npmjs.com/package/inquirer
-
-var inquirer = require('inquirer');
-inquirer.prompt(["Enter something"]).then(function (answers) {
-	// Use user feedback for... whatever!! 
-	console.log(answers);
-});
